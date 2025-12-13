f5cbd46d9ea6cced46da7bb5ab03c4ab61117bc4
“Haze” <12jeansmith21@gmail.com>
2025-12-13 18:31:37 +0800

    deleted the delete button in admin

--- app.py
+++ app.py
@@ -116,4 +116,4 @@ def finance_dashboard():
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 50001)), debug=True)
